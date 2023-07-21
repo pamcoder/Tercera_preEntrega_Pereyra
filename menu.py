3f99c884e146b3efd135a5450e123bef541a5872
Pame Pereyra <138718813+pamcoder@users.noreply.github.com>
2023-07-21 13:28:02 -0300

    Delete menu.py

--- menu.py
+++
@@ -1,7 +0,0 @@
-from mi_primer_paquete.modulo_1 import Persona
-from mi_primer_paquete.modulo_2 import llamado_modulo2
-
-persona1 =  Persona("Pame", "Pereyra")
-print(persona1)
-
-llamado_modulo2()
