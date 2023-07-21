a1185eb69a92cf4ebd4f9d5c77996e5f239c0bc9
Pame Pereyra <138718813+pamcoder@users.noreply.github.com>
2023-07-21 13:26:12 -0300

    Delete Alumno.py

--- Alumno.py
+++
@@ -1,9 +0,0 @@
-class Alumno:
-    def __init__(self,nombre,nota):
-        self.nombre = nombre
-        self.nota = nota
-    
-    def imprimir(self):
-       print('Nombre: ', self.nombre, '\nNota: ', self.nota)
-    
-   
